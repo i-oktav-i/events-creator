9eac79f95fab8955eeaf10848e587cc2fb5c396d
a.taranenko <i_okatv_i@mail.ru>
2024-08-27 17:42:17 +0300

    feat: Hash routing added

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,46 +1,41 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
+import { createHashRouter, redirect } from 'react-router-dom';
 
 import { GraphPage } from '@pages/Graph';
 import { CreatePage, EditPage, MainPage } from '@pages/gameEvents';
 import { Layout } from './ui/Layout';
 
-export const router = createBrowserRouter(
-  [
-    {
-      element: <Layout />,
-      children: [
-        {
-          path: '/',
-          children: [
-            {
-              path: '',
-              element: <MainPage />,
-            },
-            {
-              path: 'create/',
-              element: <CreatePage />,
-            },
-            {
-              path: 'edit/',
-              loader: () => redirect('..'),
-            },
-            {
-              path: 'edit/:id',
-              element: <EditPage />,
-            },
-          ],
-        },
-        {
-          path: '/game/',
-        },
-        {
-          path: '/graph/',
-          element: <GraphPage />,
-        },
-      ],
-    },
-  ],
+export const router = createHashRouter([
   {
-    basename: import.meta.env.BASE_URL,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        children: [
+          {
+            path: '',
+            element: <MainPage />,
+          },
+          {
+            path: 'create/',
+            element: <CreatePage />,
+          },
+          {
+            path: 'edit/',
+            loader: () => redirect('..'),
+          },
+          {
+            path: 'edit/:id',
+            element: <EditPage />,
+          },
+        ],
+      },
+      {
+        path: '/game/',
+      },
+      {
+        path: '/graph/',
+        element: <GraphPage />,
+      },
+    ],
   },
-);
+]);
