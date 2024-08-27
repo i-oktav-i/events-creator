7e9295b4975815462ca8a9c0ac94ae9367f7faed
a.taranenko <i_okatv_i@mail.ru>
2024-08-27 15:42:40 +0300

    fix: Graph container size fix

--- src/pages/Graph/Graph.css.ts
+++ src/pages/Graph/Graph.css.ts
@@ -1,8 +1,16 @@
 import { container } from '@shared/theme';
+import { style } from '@vanilla-extract/css';
 
 export const pageContainer = container({ size: 'full', display: 'flex' });
 
-export const fullSizeContainer = container({ size: 'full' });
+export const fullSizeContainer = style({
+  selectors: {
+    '&&': {
+      width: '100%',
+      height: '100%',
+    },
+  },
+});
 
 export const formContainer = container({
   width: 'twoThird',
